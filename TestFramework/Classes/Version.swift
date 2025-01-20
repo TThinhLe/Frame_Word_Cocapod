36cdc3b50f2eb174f1f22e9153a35cb3c653eb25
thinhtle <thinht.le@spiraledge.com>
2025-01-20 20:14:02 +0700

    Update code before creating new tag 0.1.0

--- TestFramework/Classes/Version.swift
+++ TestFramework/Classes/Version.swift
@@ -12,20 +12,6 @@ public class Version {
     public init() {}
 
     public func sayHello() -> String {
-        let podfileLockPath = Bundle.main.path(forResource: "Podfile", ofType: "lock") ?? ""
-        guard let content = try? String(contentsOfFile: podfileLockPath, encoding: .utf8) else {
-            return ""
-        }
-        
-        let lines = content.components(separatedBy: "\n")
-        for line in lines {
-            if line.contains("TestFramework") {
-                let components = line.split(separator: " ")
-                if components.count > 1 {
-                    print(components.last?.trimmingCharacters(in: .punctuationCharacters) ?? "")
-                }
-            }
-        }
-        return ""
+       return "0.6.0"
     }
 }
